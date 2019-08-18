354b9dbbc8b5b6fe89a81797133a0ec66dac8165
99Kies <1290017556@qq.com>
2019-08-18 16:56:25 +0800

    selenium访m_csdn_visitor

--- Vistor/slum_csdn.py
+++ Vistor/slum_csdn.py
@@ -9,10 +9,8 @@ import re
 
 
 proxy = [
-    '106.12.18.45:8899',
-    # '106.15.47.79:8899',
-    '106.12.187.224:8899',
-    '118.25.113.115:8899',
+    'xxxx.xxxx.xxx:xxx',
+    'xxx.xxx.xxx.xx:xxxx'
 ]
 
 def count_run(i):
@@ -23,8 +21,6 @@ def count_run(i):
         options.add_argument('--headless')
         browser = webdriver.Chrome(chrome_options=options)
         wait = WebDriverWait(browser, 10)
-        # print(proxy[i%4])
-        # print('try',i)
         browser.get('https://blog.csdn.net/qq_19381989/article/details/95764431')
         read_count = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,'read-count')))
         print(read_count.text)
@@ -34,17 +30,5 @@ def count_run(i):
         # print(read_count.text)
         print(i)
 
-# count_run()
 for i in range(10990):
     count_run(i)
-
-# wait = WebDriverWait(browser,10)
-# read_count = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,'read-count')))
-
-# print(group)
-# if __name__ == '__main__':
-#     group = [i for i in range(1000)]
-#     pool = Pool()
-#     pool.map(count_run,group)
-#     pool.close()
-#     pool.join()
